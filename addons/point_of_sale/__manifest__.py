2de9f4a446998dfff31dcf8cec3d486eb48c1d8f
Marco Tosato <marco.tosato@didotech.com>
2023-03-21 11:48:56 +0100

    [VERSION] point_of_sale: aggiornato numero di versione

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale',
-    'version': '1.0.1_a',
+    'version': '1.0.1_b',
     'category': 'Point Of Sale',
     'sequence': 20,
     'summary': 'Tablet POS: shops and restaurants',
