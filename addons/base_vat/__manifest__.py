d6cd63c3bf725f7e59ae03f72f486bc9ba8667ba
Andrei Levin <andrei.levin@codebeex.com>
2024-10-25 17:51:42 +0200

    [IMP] base_vat: added correct VAT check for Germany, including generic VAT (999999999)

--- addons/base_vat/__manifest__.py
+++ addons/base_vat/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+# noinspection PyStatementEffect
 {
     'name': 'VAT Number Validation',
-    'version': '1.0',
+    'version': '1.0a',
     'category': 'Accounting',
     'description': """
 VAT validation for Partner's VAT numbers.
